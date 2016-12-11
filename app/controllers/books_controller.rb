d398b602450617b5e353e1d8b64552fd43e710f5
msosborne3 <msosborne@ymail.com>
2016-12-11 00:06:22 -0600

    added get '/books/:slug' route to BooksController

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,3 +1,13 @@
 class BooksController < ApplicationController
-  
+  # Loads the book page for a specific book
+  get '/books/:slug' do
+    if logged_in? # If the user is logged in, they can view a book page
+      @book = Book.find_by_slug(params[:slug])
+
+      erb :'/books/show'
+    else
+      redirect to '/login' # Otherwise, they're redirected to the login page
+    end
+  end
+
 end
