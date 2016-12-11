c360f9ea6a79f693f6fc07d6d6e3e021cc988920
msosborne3 <msosborne@ymail.com>
2016-12-11 00:46:54 -0600

    Add comments for routes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,25 +10,30 @@ class BooksController < ApplicationController
     end
   end
 
+  # Loads the page to add/create new books
   get '/books/new' do
-    if logged_in?
+    if logged_in? # If the user is logged in, they can add a book
       erb :'/books/create_book'
     else
-      redirect to '/login'
+      redirect to '/login' # Otherwise, they're redirected to the login page
     end
   end
 
+  # Gets the input from the new book page
   post '/books' do
     @user = current_user
 
+    # If the user checks any current books, they're added to the user's booklist
     if params[:books]
       @user.books << params[:books]
     end
 
+    # If the user enters a new book, it is created and added to the booklist
     if params[:name] != "" && params[:author] != "" && params[:genre] != ""
       current_user.books.create(name: params[:name], author: params[:author], genre: params[:genre])
     end
 
+    # The user is then redirected to their profile page with their booklist
     redirect to "/users/#{current_user.slug}"
   end
 
