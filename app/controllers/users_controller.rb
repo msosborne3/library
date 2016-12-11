882e49c0d440f8a2e3c9f2d7fbcfa3fe8cc4177c
msosborne3 <msosborne@ymail.com>
2016-12-10 21:14:33 -0600

    add get '/signup' route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,13 @@
 class UsersController < ApplicationController
 
+  # Loads the create_user page, which contains a form to create a new user
+  get '/signup' do
+    if logged_in? # If a user is already logged in, then they can't sign up
+      redirect to "/users/#{current_user.id}"
+    else
+      erb :'/users/create_user'
+    end
+  end
+
+
 end
