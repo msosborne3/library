646f358d4b1388ad6aab41e82235392b884f9277
msosborne3 <msosborne@ymail.com>
2016-12-10 21:15:44 -0600

    add post '/signup' route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,5 +9,17 @@ class UsersController < ApplicationController
     end
   end
 
+  # Gets the input from the form and creates a new user
+  post '/signup' do
+    # If the user did not enter a username, email, and password then they have to fill out the form again
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect to '/signup'
+    else  # Otherwise a new user is created with the info and the user is then redirected to their profile page
+      @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+      session[:user_id] = @user.id
+      redirect to "/users/#{current_user.id}"
+    end
+  end
+
 
 end
