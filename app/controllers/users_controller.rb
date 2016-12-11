1091bbfbef8816d3bbc04fbe32d1451b97156a41
msosborne3 <msosborne@ymail.com>
2016-12-10 21:20:39 -0600

    add get '/users/:slug' route to show profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
 
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :"users/show"
+  end
+
   # Loads the create_user page, which contains a form to create a new user
   get '/signup' do
     if logged_in? # If a user is already logged in, then they can't sign up
-      redirect to "/users/#{current_user.id}"
+      redirect to "/users/#{current_user.slug}"
     else
       erb :'/users/create_user'
     end
@@ -17,9 +22,11 @@ class UsersController < ApplicationController
     else  # Otherwise a new user is created with the info and the user is then redirected to their profile page
       @user = User.create(username: params[:username], email: params[:email], password: params[:password])
       session[:user_id] = @user.id
-      redirect to "/users/#{current_user.id}"
+      redirect to "/users/#{@user.slug}"
     end
   end
+
+
 
 
 end
