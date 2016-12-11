ee76d88b583531de3a7529483f71243aaf4a5983
msosborne3 <msosborne@ymail.com>
2016-12-10 21:19:13 -0600

    add slug method and find by slug method to user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  # Converts a username from "test 123" to "test-123"
+  def slug
+    username.downcase.gsub(' ', '-')
+  end
+
+  # Finds the user by the slugified version of its username
+  def self.find_by_slug(slug)
+    User.all.find{|user| user.slug == slug}
+  end
+
 end
