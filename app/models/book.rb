fbcf3affd95d6c599240ebbbd177a4a6e8213695
msosborne3 <msosborne@ymail.com>
2016-12-11 00:07:53 -0600

    add slug and find_by_slug methods to the Book class

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,14 @@
 class Book < ActiveRecord::Base
   has_many :user_books
   has_many :users, :through => :user_books
+
+  # Converts a book title from "Harry Potter" to "harry-potter"
+  def slug
+    name.downcase.gsub(' ', '-')
+  end
+
+  # Finds the book by the slugified version of its name
+  def self.find_by_slug(slug)
+    Book.all.find{|book| book.slug == slug}
+  end
 end
